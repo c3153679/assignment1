db8545c07935132cd556e2c51bc7968e6b2cd72e
Pat shaw <c3153679@uon.edu.au>
2019-04-10 09:25:35 +0000

    push changes to git 6:52 10/4 push changes to git 7:24 10/4

--- main.c
+++ main.c
@@ -1,7 +1,59 @@
-#include <stdio.h>
-/* initiation of assignment 1 for geng1003 Patrick Shaw c3153679 */
+/* Main function of the C program. */
 
-int main(void)
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void encrypt (char*x, int key);
+int main()
 {
-    printf("hello world")
+   // declare char message for encoding//
+   
+   char message[10000];
+   int key; // rotation value cipher key//
+   int i = 0; //loop counter//
+   
+   // make function to ascii convert lower to upper case.//
+   
+               
+   printf("enter a message you want to have encrypted\n");
+   scanf("%[^\n]", message);
+   printf("please enter the encryption key\n""enter number between 1 & 26\n");
+   scanf("%d",&key);
+    for (i = 0; message[i]!='\0'; i++) 
+                    {
+                    if(message[i] >= 'a' && message[i] <= 'z') {
+                    message[i] = message[i] -32;
+                    }
+                    }
+printf("un-encrypted message: %s\n", message);       //test case change//
+encrypt (message, key);
+printf("encrypted message: %s\n", message);
+
 }
+
+void encrypt(char*x,int key)
+{int i = 0;
+while (x[i]!='\0')
+{ 
+x[i] = (x[i]-65 + key)%26 +65;
+i = i++;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
